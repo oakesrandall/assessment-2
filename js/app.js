803e01c59a27534ea9d65759f5c185cd8b8c1c5f
David_Randall <oakesrandall@gmail.com>
2017-07-10 07:13:43 -0600

    resized images, added backgrounds, created href for button

--- js/app.js
+++ js/app.js
@@ -2,4 +2,24 @@ $(function() {
 	console.log("page loaded");
 });
 
+
 //make objects with constructor
+
+// function GameState() {
+// 	this.carOne = 0;
+// 	this.carTwo = 0;
+// 	this.winningCondition = 20;
+// 	// this.hasWon = function() {
+// 	// 	if (GameState.carOne === GameState.winningCondition
+
+// }
+
+
+
+
+
+// var hasWon = new Gamestate (hasWon, (function() {
+// 	if 
+// });
+
+//  
