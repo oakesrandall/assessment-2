5c1eda0fb4ceeb8396b59f51607bb5296a3654af
David_Randall <oakesrandall@gmail.com>
2017-07-11 16:00:22 -0600

    game working, need to make responsive

--- js/app.js
+++ js/app.js
@@ -8,20 +8,32 @@ $(document).keydown(function(event) {
 	if (event.which === 90){
 		carOneMargin += 5;
 		$(".carOneImg").css("margin-left", carOneMargin + "%");
+		hasWon(); 
 	}
 	else if (event.which === 191){
 		carTwoMargin += 5;
 		$(".carTwoImg").css("margin-left", carTwoMargin + "%");
+		hasWon(); 
 	}
 
 });
 
+
+
+function resetGame() {
+	carOneMargin = 0;
+	carTwoMargin = 0;
+	$(".carOneImg").css("margin-left", carOneMargin);
+	$(".carTwoImg").css("margin-left", carTwoMargin);
+}
+
 function hasWon() {
 	if (carOneMargin === 90) {
 		alert("Car One has won the race!");
+		resetGame();
 	} 
 	else if (carTwoMargin === 90) {
 		alert("Car Two has won the race!");
+		resetGame();
 	}
 }
-
