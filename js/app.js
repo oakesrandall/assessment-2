f9312c54289a654354a6cc28ba679e409e28f94e
David_Randall <oakesrandall@gmail.com>
2017-07-11 16:03:41 -0600

    added player objects

--- js/app.js
+++ js/app.js
@@ -1,38 +1,40 @@
-var carOneMargin = 0;
-var carTwoMargin = 0;
+var carOne = {
+	margin: 0
+};
+var carTwo = {
+	margin: 0
+};
 
 $(document).ready(function() {
 });
 
 $(document).keydown(function(event) {
 	if (event.which === 90){
-		carOneMargin += 5;
-		$(".carOneImg").css("margin-left", carOneMargin + "%");
+		carOne.margin += 5;
+		$(".carOneImg").css("margin-left", carOne.margin + "%");
 		hasWon(); 
 	}
 	else if (event.which === 191){
-		carTwoMargin += 5;
-		$(".carTwoImg").css("margin-left", carTwoMargin + "%");
+		carTwo.margin += 5;
+		$(".carTwoImg").css("margin-left", carTwo.margin + "%");
 		hasWon(); 
 	}
 
 });
 
-
-
 function resetGame() {
-	carOneMargin = 0;
-	carTwoMargin = 0;
-	$(".carOneImg").css("margin-left", carOneMargin);
-	$(".carTwoImg").css("margin-left", carTwoMargin);
+	carOne.margin = 0;
+	carTwo.margin = 0;
+	$(".carOneImg").css("margin-left", carOne.margin);
+	$(".carTwoImg").css("margin-left", carTwo.margin);
 }
 
 function hasWon() {
-	if (carOneMargin === 90) {
+	if (carOne.margin === 90) {
 		alert("Car One has won the race!");
 		resetGame();
 	} 
-	else if (carTwoMargin === 90) {
+	else if (carTwo.margin === 90) {
 		alert("Car Two has won the race!");
 		resetGame();
 	}
