3c0b32ed5954e9fface504224d3efc90941bb77a
David_Randall <oakesrandall@gmail.com>
2017-07-12 11:05:43 -0600

    added comments

--- js/app.js
+++ js/app.js
@@ -1,27 +1,31 @@
+//Declares variables to use to move the cars
 var carOne = {
 	margin: 0
 };
 var carTwo = {
 	margin: 0
 };
-
+//Check to see if the page is ready
 $(document).ready(function() {
 });
 
+//Listens for keystrokes and increases the margin of the
+//corresponding car
 $(document).keydown(function(event) {
 	if (event.which === 90){
 		carOne.margin += 5;
 		$(".carOneImg").css("margin-left", carOne.margin + "%");
-		hasWon(); 
+		hasWon();
 	}
 	else if (event.which === 191){
 		carTwo.margin += 5;
 		$(".carTwoImg").css("margin-left", carTwo.margin + "%");
-		hasWon(); 
+		hasWon();
 	}
 
 });
 
+//resets the game by resetting the margin and moving the cars there
 function resetGame() {
 	carOne.margin = 0;
 	carTwo.margin = 0;
@@ -29,11 +33,13 @@ function resetGame() {
 	$(".carTwoImg").css("margin-left", carTwo.margin);
 }
 
+//determines if the car has reached a specific point and declares
+//a winner
 function hasWon() {
 	if (carOne.margin === 90) {
 		alert("Car One has won the race!");
 		resetGame();
-	} 
+	}
 	else if (carTwo.margin === 90) {
 		alert("Car Two has won the race!");
 		resetGame();
